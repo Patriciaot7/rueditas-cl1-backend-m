68459d91fd0870417b07810c40e74bb761ceefd7
Patricia <patriciaevelyn2005@gmail.com>
2024-09-26 12:53:31 -0500

    Carga inicial de backend

---
+++ src/main/java/pe/edu/cibertec/rueditas_cl1_backend/RueditasCl1BackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_cl1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasCl1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasCl1BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/rueditas_cl1_backend/RueditasCl1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_cl1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RueditasCl1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
